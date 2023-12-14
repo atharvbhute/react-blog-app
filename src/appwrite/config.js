62e0a5579100700a1f67ff86dfb49339eb4c5bea
Atharv Bhute <atharv.bhute18@gmail.com>
2023-12-14 17:58:05 +0530

    store updated

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -1,4 +1,4 @@
-import { Client, Databases } from "appwrite";
+import { Client, Databases, Query } from "appwrite";
 import conf from "../conf/conf";
 
 class Service {
@@ -12,11 +12,15 @@ class Service {
       .setProject(conf.appwriteProjectId);
 
     this.databases = new Databases(this.client);
+    this.bucket = new Storage(this.client);
   }
 
   async createPost({ title, content, featuredImage, status, userId, slug }) {
     try {
-      return this.databases.createDocument(conf.appwritePDatabaseId, conf.appwriteCollectionId, slug,
+      return this.databases.createDocument(
+        conf.appwritePDatabaseId,
+        conf.appwriteCollectionId,
+        slug,
         { title, content, featuredImage, status, userId }
       );
     } catch (error) {
@@ -24,46 +28,88 @@ class Service {
     }
   }
 
-  async updatePost(slug, {title, content, featuredImage, status}){
+  async updatePost(slug, { title, content, featuredImage, status }) {
     try {
-        return await this.databases.updateDocument(conf.appwritePDatabaseId, conf.appwriteCollectionId, slug, 
-            {title, content, featuredImage, status}
-         );
+      return await this.databases.updateDocument(
+        conf.appwritePDatabaseId,
+        conf.appwriteCollectionId,
+        slug,
+        { title, content, featuredImage, status }
+      );
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async deletePost({ slug }) {
+    try {
+      await this.databases.deleteDocument(
+        conf.appwritePDatabaseId,
+        conf.appwriteCollectionId,
+        slug
+      );
+      return true;
+    } catch (error) {
+      console.log("Error: ", error);
+      return false;
+    }
+  }
+
+  async getAllPosts(query = [Query.equal('Status', 'active')]) { // get's only active posts
+    
+    try {
+      const allPosts = await this.databases.listDocuments(
+        conf.appwritePDatabaseId,
+        conf.appwriteCollectionId,
+        query
+      );
+      return allPosts;
+    } catch (error) {
+      console.log("error : ", error);
+      return false;
+    }
+  }
+
+  async getPost(slug) {
+    try {
+      const post = await this.databases.getDocument(
+        conf.appwritePDatabaseId,
+        conf.appwriteCollectionId,
+        slug
+      );
+      return post;
+    } catch (error) {
+      console.log("error : ", error);
+      return false;
+    }
+  }
+
+  async uploadImage(file) {
+    try {
+      return await this.bucket.createFile(
+        conf.appwriteBucketId,
+        ID.unique(),
+        file
+      );
+    } catch (error) {
+      throw error;
+    }// delete image
+  }
+
+  async deleteImage(fileId){
+    try {
+       await this.bucket.deleteFile(conf.appwriteBucketId, fileId);        
+       return true;
     } catch (error) {
         throw error        
     }
   }
 
-  async deletePost({slug}){
-    try {
-        await this.databases.deleteDocument(conf.appwritePDatabaseId, conf.appwriteCollectionId, slug)
-        return true
-    } catch (error) {
-        console.log("Error: ", error);
-        return false
-    }
+  async getImagePreview(fileId){
+    const imagePreview = storage.getFilePreview(conf.appwriteBucketId, fileId);
+    return imagePreview;
   }
-
-  async getAllPosts(user){ // build this using query query(status == active) 
-    try {
-        const allPosts =  await this.databases.listDocuments(conf.appwritePDatabaseId, conf.appwriteCollectionId);
-        return allPosts;
-    } catch (error) {
-        console.log("error : ", error);  
-        return false    
-    }
-  }
-
-  async getPost(slug){
-    try {
-        const post = this.databases.getDocument(conf.appwritePDatabaseId, conf.appwriteCollectionId, slug);   
-        return post;     
-    } catch (error) {
-        console.log("error : ", error); 
-        return false;
-    }
-  }
-
-  // upload image
-  // delete image
 }
+
+const service = new Service();
+export default service;
---
+++ src/store/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+const blogStore = configureStore({
+    reducer: {
+
+    }
+});
+
+export default blogStore;
