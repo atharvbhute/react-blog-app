e40d4b158cefdbe366a3728420fedc030d1b9c52
Atharv Bhute <atharv.bhute18@gmail.com>
2023-12-24 02:28:24 +0530

    components

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,35 @@ import { Header, Footer } from "./components/index";
 import { useDispatch, useSelector } from "react-redux";
 import authService from "./appwrite/auth_service";
 import { login, logout } from "./store/authSlice";
-import { Outlet } from "react-router-dom";
+import { BrowserRouter, Outlet } from "react-router-dom";
+
 
 function App() {
   const [loader, setLoader] = useState(true);
   const dispatch = useDispatch();
 
   useEffect(()=>{
-      if(authService.getCurrentUser() == null){
-        console.log("user not found");
-        dispatch(logout());
-      }else{
-        authService.getCurrentUser().then((userData) => dispatch(login(userData)))
-        .catch((err) => console.log("err: ", err))        
-      }
-      setLoader(false);
+      authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+        dispatch(login(userData));
+        console.log(userData);        
+        }else{
+          dispatch(logout());
+        }
+      }).finally(()=>{
+        setLoader(false);
+      })
   },[]);
 
   return !loader ? (
+    <BrowserRouter>
     <div>
       <Header />
       <Footer />
     </div>
+    </BrowserRouter>
+    
   ) : (
     <>
       <h1>Loading....</h1>
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,8 @@
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
+import Logo from "./Logo";
+import LogoutBtn from "./header/LogoutBtn";
+import Container from "./container/Container";
 
-export {Header, Footer}
+
+export {Header, Footer, Logo, LogoutBtn, Container}
